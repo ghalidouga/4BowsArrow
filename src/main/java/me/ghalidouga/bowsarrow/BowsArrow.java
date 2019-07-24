2f2d1851ab121017c4875679bc3e3f12316f55af
ghalidouga <ghalighalidouga@gmail.com>
2019-07-24 12:09:09 +0700

    ngasih ngambil bow saat ready unready

--- src/main/java/me/ghalidouga/bowsarrow/BowsArrow.java
+++ src/main/java/me/ghalidouga/bowsarrow/BowsArrow.java
@@ -5,13 +5,16 @@ package me.ghalidouga.bowsarrow;
 import me.ghalidouga.bowsarrow.Events.ArrowHit;
 import me.ghalidouga.bowsarrow.Events.CantBreak;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.event.server.BroadcastMessageEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.NumberConversions;
 
@@ -37,9 +40,18 @@ public final class BowsArrow extends JavaPlugin implements Listener {
             if(sender instanceof  Player) {
                 Player player = (Player) sender;
                 getServer().broadcastMessage(player.getDisplayName() + ChatColor.GREEN + " Telah Ready");
-                //player.sendMessage(player.getDisplayName() + ChatColor.GREEN + " Telah Ready");
+                player.getInventory().addItem(new ItemStack(Material.BOW,1));
+
             }else{
                 System.out.println("You need to be a player");
+            }
+        }
+
+        if(command.getName().equals("unready")){
+            if(sender instanceof  Player){
+                Player player = (Player) sender;
+                getServer().broadcastMessage(player.getDisplayName() + ChatColor.RED + " tidak jadi Ready");
+                player.getInventory().removeItem(new ItemStack(Material.BOW,1));
             }
         }
 
